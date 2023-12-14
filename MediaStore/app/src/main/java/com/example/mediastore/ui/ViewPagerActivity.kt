42fcca47e34087005b989bf3c51b6b619aeed073
pangshiming <psm@gmail.com>
2023-12-14 18:15:32 +0800

    feat: 文字轮播实现动画效果

--- MediaStore/app/src/main/java/com/example/mediastore/ui/ViewPagerActivity.kt
+++ MediaStore/app/src/main/java/com/example/mediastore/ui/ViewPagerActivity.kt
@@ -1,5 +1,8 @@
 package com.example.mediastore.ui
 
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
@@ -8,6 +11,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.addListener
 import androidx.core.view.size
 import androidx.databinding.DataBindingUtil
 import com.example.mediastore.R
@@ -20,7 +24,14 @@ import java.util.zip.Inflater
 class ViewPagerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityViewPagerBinding
     var index = 0
-    @SuppressLint("SetTextI18n", "InflateParams")
+    var isStart = true
+    val animatorSet = AnimatorSet()
+    var objectAnimatorOut: ObjectAnimator? = null
+    private var objectAnimatorX: ObjectAnimator? = null
+    private var objectAnimatorIn: ObjectAnimator? = null
+//    val objectAnimatorX = ObjectAnimator.ofFloat(view, "x", 220f)
+
+    @SuppressLint("SetTextI18n", "InflateParams", "Recycle")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val text1 = LayoutInflater.from(this).inflate(R.layout.text_row_item, null)
@@ -34,14 +45,42 @@ class ViewPagerActivity : AppCompatActivity() {
         var view: View
         binding.vf.stopFlipping()
         binding.vf.removeAllViews()
+        animatorSet.duration = 2000
         for (i in 0..2) {
             Log.d("TAG", "onCreate: $index")
             Log.d("TAG", "onCreate: ${binding.vf.size}")
             index++
             view = LayoutInflater.from(this).inflate(R.layout.text_row_item, null)
             view.findViewById<TextView>(R.id.textView).text = "text$index"
+
             binding.vf.addView(view)
         }
+        setStartAnimator(binding.vf)
+        setStartAnimator(binding.btAddView)
+        animatorSet.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator?) {
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                isStart = !isStart
+                if(isStart){
+                    setStartAnimator(binding.btAddView)
+                    setStartAnimator(binding.vf)
+                }else{
+                    setEndAnimator(binding.btAddView)
+                    setEndAnimator(binding.vf)
+                }
+                animatorSet.start()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+
+        })
+        animatorSet.start()
         binding.btAddView.setOnClickListener {
 
 
@@ -57,5 +96,23 @@ class ViewPagerActivity : AppCompatActivity() {
         }
         val viewList = listOf<View>(text1, text2, text3)
 
+    }
+
+    fun setStartAnimator(view: View) {
+         objectAnimatorIn = ObjectAnimator.ofFloat(view, "alpha", 0f, 1f)
+        animatorSet.play(objectAnimatorX).with(objectAnimatorIn)
+    }
+
+    fun setEndAnimator(view: View) {
+         objectAnimatorOut = ObjectAnimator.ofFloat(view, "alpha", 1f, 0f)
+         objectAnimatorX = ObjectAnimator.ofFloat(view, "x", 220f,0f)
+        animatorSet.play(objectAnimatorOut).with(objectAnimatorX)
+    }
+
+    override fun onDestroy() {
+        animatorSet.cancel()
+        animatorSet.end()
+        animatorSet.removeAllListeners()
+        super.onDestroy()
     }
 }
